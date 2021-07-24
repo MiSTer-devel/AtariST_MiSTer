b07f4f0ce4604ced92612fe0212305b45b4c3cf6
sorgelig <pour.garbage@gmail.com>
2021-07-24 20:48:59 +0800

    Update FX68K to latest.

--- rtl/fx68k/fx68k.sv
+++ rtl/fx68k/fx68k.sv
@@ -2,7 +2,7 @@
 // FX68K
 //
 // M68000 cycle accurate, fully synchronous
-// Copyright (c) 2018 by Jorge Cwik
+// Copyright (c) 2018,2021 by Jorge Cwik
 // 
 // TODO:
 // - Everything except bus retry already implemented.
@@ -11,6 +11,13 @@
 // altera message_off 10230
 
 `timescale 1 ns / 1 ns
+
+//`define USE_E_CLKEN
+/*
+	Define USE_E_CLKEN will output two signals that generate a single cycle pulse just before the raising and falling edges of E.
+	Use this when you need to generate changes that must be simultaneous with these edges.
+	Most systems don't need this. Note that these signals are not registered. 
+ */
 
 // Define this to run a self contained compilation test build
 // `define FX68K_TEST
@@ -131,6 +138,8 @@ typedef struct {
 
 module fx68k(
 	input clk,
+	input HALTn,					// Used for single step only. Force high if not used
+	// input logic HALTn = 1'b1,			// Not all tools support default port values
 	
 	// These two signals don't need to be registered. They are not async reset.
 	input extReset,			// External sync reset on emulated system
@@ -138,7 +147,13 @@ module fx68k(
 	input enPhi1, enPhi2,	// Clock enables. Next cycle is PHI1 or PHI2
 
 	output eRWn, output ASn, output LDSn, output UDSn,
-	output logic E, output VMAn,	
+	output logic E, output VMAn,
+	
+`ifdef USE_E_CLKEN
+	// Next cycle would be raising/falling edge of E output
+	output E_PosClkEn, E_NegClkEn,
+`endif	
+
 	output FC0, output FC1, output FC2,
 	output BGn,
 	output oRESETn, output oHALTEDn,
@@ -195,8 +210,8 @@ module fx68k(
 	// We synchronize some signals half clock earlier. We compensate later
 	reg rDtack, rBerr;
 	reg [2:0] rIpl, iIpl;
-	reg Vpai, BeI, BRi, BgackI, BeiDelay;
-	// reg rBR;
+	reg Vpai, BeI, Halti, BRi, BgackI, BeiDelay;
+	// reg rBR, rHALT;
 	wire BeDebounced = ~( BeI | BeiDelay);
 
 	always_ff @( posedge Clks.clk) begin
@@ -210,16 +225,20 @@ module fx68k(
 			rIpl <= ~{ IPL2n, IPL1n, IPL0n};
 			iIpl <= rIpl;
 			
-			// rBR <= BRn;			// Needed for cycle accuracy but only if BR is changed on the wrong edge of the clock
+			// Needed for cycle accuracy but only if BR or HALT are asserted on the wrong edge of the clock			
+			// rBR <= BRn;
+			// rHALT <= HALTn;
 		end
 		else if( Clks.enPhi1) begin
 			Vpai <= VPAn;
 			BeI <= rBerr;
 			BeiDelay <= BeI;
+			BgackI <= BGACKn;
 			
 			BRi <= BRn;
-			BgackI <= BGACKn;
+			Halti <= HALTn;
 			// BRi <= rBR;
+			// Halti <= rHALT;			
 		end	
 	end
 
@@ -356,7 +375,7 @@ module fx68k(
 		.rDtack, .BeDebounced, .Vpai,
 		.ASn, .LDSn, .UDSn, .eRWn);
 		
-	busArbiter busArbiter( .Clks, .BRi, .BgackI, .Halti( 1'b1), .bgBlock, .busAvail, .BGn);
+	busArbiter busArbiter( .Clks, .BRi, .BgackI, .Halti, .bgBlock, .busAvail, .BGn);
 		
 		
 	// Output reset & halt control
@@ -457,6 +476,11 @@ module fx68k(
 	
 	// Internal stop just one cycle before E falling edge
 	wire xVma = ~rVma & (eCntr == 8);
+	
+`ifdef USE_E_CLKEN
+	assign E_PosClkEn = (Clks.enPhi2 & (eCntr == 5));
+	assign E_NegClkEn = (Clks.enPhi2 & (eCntr == 9));
+`endif
 	
 	always_ff @( posedge Clks.clk) begin
 		if( Clks.pwrUp) begin
@@ -1774,9 +1798,10 @@ endmodule
 // Also checks for illegal opcode and priv violation
 
 // This is one of the slowest part of the processor.
-// But no need to optimize or pipeline because the result is not needed until at least 4 cycles.
+// But no need to optimize or pipeline because the result for a1-a3 is not needed until at least 4 cycles.
 // IR updated at the least one microinstruction earlier.
 // Just need to configure the timing analizer correctly.
+// isPriv, isIllegal might be needed as soon as two cycles later
 
 module uaddrDecode( 
 	input [15:0] opcode,
